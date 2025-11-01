feb880c8d504da033a45315dac0d17a814084384
Filip Dragović <dr.ipi95@gmail.com>
2025-11-01 01:01:52 +0100

    update: crawlLinks set to true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,7 +51,7 @@ export default defineNuxtConfig({
     },
     prerender: {
       routes: ['/footer-content', '/mrid'],
-      crawlLinks: false,
+      crawlLinks: true,
       failOnError: false,
     },
   },
